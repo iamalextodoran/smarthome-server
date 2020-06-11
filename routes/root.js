effcb45c982261618430fac1a24cd0be96d5898a
Alex Todoran <todoran_alex@yahoo.com>
2020-06-11 20:47:58 +0300

    Fetching per single user data

---
+++ routes/root.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const { User, Room, Device } = require("../models");
+
+getUser = (req, res) => {
+  User.findByPk(req.params.UserId, {
+    include: [{ model: Room }],
+  })
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).send(err));
+};
+
+getUserRooms = (req, res) => {
+  Room.findAll({
+    where: {
+      UserId: req.params.UserId,
+    },
+    include: Device,
+  })
+    .then((rooms) => res.status(200).json(rooms))
+    .catch((err) => res.status(400).send(err));
+};
+
+getRoom = (req, res) => {
+  Room.findAll({
+    where: {
+      id: req.params.RoomId,
+    },
+    include: Device,
+  })
+    .then((rooms) => res.status(200).json(rooms))
+    .catch((err) => res.status(400).send(err));
+};
+
+getUserDevices = (req, res) => {
+  Device.findAll({
+    where: {
+      RoomId: req.params.UserId,
+    }
+  })
+    .then((rooms) => res.status(200).json(rooms))
+    .catch((err) => res.status(400).send(err));
+};
+
+router.get("/:UserId", getUser);
+router.get("/:UserId/rooms", getUserRooms);
+router.get("/:UserId/rooms/:RoomId", getRoom);
+router.get("/:UserId/devices", getUserDevices);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Root path'));
+app.use('/', require('./routes/root'));
 app.use('/users', require('./routes/users'))
 app.use('/rooms', require('./routes/rooms'))
 app.use('/devices', require('./routes/devices'))
