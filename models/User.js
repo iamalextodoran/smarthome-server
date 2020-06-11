714c764076e22cf9e19b61a24554795645ca6c7e
Alex Todoran <todoran_alex@yahoo.com>
2020-06-11 20:12:29 +0300

    City column

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     temperatureMode: DataTypes.STRING,
     temperature: DataTypes.INTEGER,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    city: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     User.hasMany(models.Room);
