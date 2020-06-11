fcabddca4582b2d3a5929f1a912b8182a2358ff5
Alex Todoran <todoran_alex@yahoo.com>
2020-06-11 22:24:12 +0300

    New api route

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/', require('./routes/root'));
-app.use('/users', require('./routes/users'))
-app.use('/rooms', require('./routes/rooms'))
-app.use('/devices', require('./routes/devices'))
+app.get("/", (res) => res.send("Root path"));
+app.use("/api", require("./routes/root"));
+app.use("/users", require("./routes/users"));
+app.use("/rooms", require("./routes/rooms"));
+app.use("/devices", require("./routes/devices"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`))
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
